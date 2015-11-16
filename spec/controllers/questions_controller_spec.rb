550664cb133d5ea3a63b8d91d1053321c8555e61
Dmitry Yarikov <yarikov@yahoo.com>
2015-11-16 14:32:38 +0300

    Questions POST #create

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { create(:question) }
+
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
     before { get :index }
@@ -15,7 +17,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe 'GET #show' do
-    let(:question) { create(:question) }
     before { get :show, id: question }
 
     it 'assigns the requested question to @question' do
@@ -36,6 +37,32 @@ RSpec.describe QuestionsController, type: :controller do
 
     it 'renders new view' do
       expect(response).to render_template :new
+    end
+  end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new question in the database' do
+        expect { post :create, question: attributes_for(:question) }
+          .to change(Question, :count).by(1)
+      end
+
+      it 'redirects to show view' do
+        post :create, question: attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the question in the database' do
+        expect { post :create, question: attributes_for(:invalid_question) }
+          .to_not change(Question, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, question: attributes_for(:invalid_question)
+        expect(response).to render_template :new
+      end
     end
   end
 end
