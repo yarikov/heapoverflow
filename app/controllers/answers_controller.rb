a8d0f864b535c222debe0791edb2158001d2615b
Dmitry Yarikov <yarikov@yahoo.com>
2015-11-16 16:14:59 +0300

    Answers POST #create

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -5,9 +5,23 @@ class AnswersController < ApplicationController
     @answer = Answer.new
   end
 
+  def create
+    @answer = @question.answers.new(answer_params)
+
+    if @question.save
+      redirect_to @question
+    else
+      render :new
+    end
+  end
+
   private
 
   def set_question
     @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -15,4 +15,30 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves the new answer in the database' do
+        expect { post :create, question_id: question, answer: attributes_for(:answer) }
+          .to change(Answer, :count).by(1)
+      end
+
+      it 'redirects to question show view' do
+        post :create, question_id: question, answer: attributes_for(:answer)
+        expect(response).to redirect_to question
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not save the answer in the database' do
+        expect { post :create, question_id: question, answer: attributes_for(:invalid_answer) }
+          .to_not change(Answer, :count)
+      end
+
+      it 're-renders new view' do
+        post :create, question_id: question, answer: attributes_for(:invalid_answer)
+        expect(response).to render_template :new
+      end
+    end
+  end
 end
