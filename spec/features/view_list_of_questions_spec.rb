451a1502cb31f90aeb240f66686511add0340110
Dmitry Yarikov <yarikov@yahoo.com>
2015-11-17 15:25:49 +0300

    User can view list of questions

---
+++ spec/features/view_list_of_questions_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+feature 'List of questions', '
+  In order to find an interesting question
+  As an user
+  I want to view list of questions
+' do
+  given!(:questions) { create_list(:question, 3) }
+
+  scenario 'User view list of questions' do
+    visit questions_path
+
+    questions.each do |question|
+      expect(page).to have_content question.title
+    end
+  end
+end
