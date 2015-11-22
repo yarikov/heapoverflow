15c188dc8666a46efd610de843c2f66cb1cd70c6
Dmitry Yarikov <yarikov@yahoo.com>
2015-11-22 09:18:25 +0300

    Answer errors

--- spec/features/answers/create_answer_spec.rb
+++ spec/features/answers/create_answer_spec.rb
@@ -19,6 +19,15 @@ feature 'Сreate an answer', '
     within('.answers') { expect(page).to have_content 'Answer body' }
   end
 
+  scenario 'Authenticated user creates invalid answer', js: true do
+    sign_in(user)
+    visit question_path(question)
+
+    click_on 'Ответить'
+
+    expect(page).to have_content "Body can't be blank"
+  end
+
   scenario 'Unauthenticated user creates an answer' do
     visit question_path(question)
     expect(page).to_not have_button('Ответить')
