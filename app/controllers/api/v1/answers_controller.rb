52c62374d4ddf219a364d6c3224960b3e230e981
Dmitry Yarikov <yarikov@yahoo.com>
2016-01-14 14:02:16 +0300

    small fix

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::AnswersController < Api::V1::BaseController
-  authorize_resource class: Answer
+  authorize_resource
 
   def create
     @question = Question.find(params[:question_id])
