43caa38d435cf0fab6b2fdcdeaec63753726dd3d
Dmitry Yarikov <dmitry@yarikov.com>
2026-05-19 18:06:12 +0300

    style(oauth): fix RuboCop Metrics/AbcSize in FindOrCreateUser
    Extract private methods to reduce ABC size of `call` method:
    - find_user_by_authorization
    - build_guest_user
    - find_or_create_authenticated_user
    - create_authorization!
    
    All 14 existing specs pass.

--- app/services/oauth/find_or_create_user.rb
+++ app/services/oauth/find_or_create_user.rb
@@ -7,21 +7,33 @@ module Oauth
     end
 
     def call
-      authorization = Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)
-      return authorization.user if authorization
-      return User.new unless auth.info&.email
-
-      user = User.find_or_create_by!(email: auth.info.email) do |u|
-        u.full_name = auth.info.name
-        u.password = Devise.friendly_token[0, 20]
-        u.skip_confirmation! if auth.credentials
-      end
-      user.authorizations.create(provider: auth.provider, uid: auth.uid)
-      user
+      find_user_by_authorization ||
+        build_guest_user ||
+        find_or_create_authenticated_user.tap { |user| create_authorization!(user) }
     end
 
     private
 
     attr_reader :auth
+
+    def find_user_by_authorization
+      Authorization.find_by(provider: auth.provider, uid: auth.uid.to_s)&.user
+    end
+
+    def build_guest_user
+      User.new unless auth.info&.email
+    end
+
+    def find_or_create_authenticated_user
+      User.find_or_create_by!(email: auth.info.email) do |user|
+        user.full_name = auth.info.name
+        user.password = Devise.friendly_token[0, 20]
+        user.skip_confirmation! if auth.credentials
+      end
+    end
+
+    def create_authorization!(user)
+      user.authorizations.create!(provider: auth.provider, uid: auth.uid)
+    end
   end
 end
